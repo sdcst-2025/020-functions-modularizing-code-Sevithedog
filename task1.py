15428305afbd94614633354901c48bc5c249f5a4
Finlay <finlay.mitchner@gmail.com>
2025-01-07 11:36:23 -0800

    conmplete

--- task1.py
+++ task1.py
@@ -1,6 +1,31 @@
+"""
+Task 1:
+Create a program with 3 function definitions:
+function A prints the message "Hello"
+function B prints the message "How are you"
+function C prints the message "Hi"
+
+Ask the user to enter a letter from A to C
+Execute the function of the letter they use.
+"""
+
 def A():
+    print("Hello")
 
 def B():
-
+    print("How are you?")
 
 def C():
+    print("Hi")
+
+
+choice = input("Pick a letter: A, B, or C: ")
+
+if choice == "A" or choice == "a":
+    A()
+elif choice == "B" or choice == "b":
+    B()
+elif choice == "C" or choice == "c":
+    C()
+else:
+    print("invalid input")
--- task2.py
+++ task2.py
@@ -8,3 +8,68 @@ displays instructions and how to play
 game()
 plays the games
 """
+import random
+import os
+import time
+def title():
+    os.system('cls')
+    print("Welcome to number guesser!")
+    time.sleep(2.5)
+    os.system('cls')
+    print("The computer will generate a random number.")
+    time.sleep(2.5)
+    os.system('cls')
+    print("You will have 3 chances to guess it.")
+    time.sleep(2.5)
+    os.system('cls')
+    print("For each incorrect guess you will get a hint.")
+    time.sleep(3.5)
+    os.system('cls')
+
+def game():
+    n = random.randint(0,100)
+    #print(n)
+    inp = False
+    for i in range(1,4):
+        while inp == False:
+            time.sleep(1)
+            os.system('cls')
+            try:
+                guess = int(input("Guess a number between 1 and 100: "))
+            except:
+                continue
+            if guess > 0 and guess < 100:
+                break
+        if guess == n:
+            print("Congrats! You guessed correct!")
+            break
+        elif i == 1:
+            if n %2 == 0:
+                print("The number is even.")
+            else: 
+                print("The number is odd")
+        elif i == 2:
+            if guess > n:
+                print("The number is less than your guess")
+            elif guess < n:
+                print("The number is greater than your guess")
+        elif i == 3: 
+            print("You have use up all your guesses")
+            print(f"The number was {n}")
+
+
+
+    
+    
+    
+
+title()
+game()
+for i in range(100000):
+    time.sleep(3)
+    os.system('cls')
+    play = input("Play again? Yes/No: ")
+    if play == "Yes" or play == "yes":
+        game()
+    else:
+        exit()
